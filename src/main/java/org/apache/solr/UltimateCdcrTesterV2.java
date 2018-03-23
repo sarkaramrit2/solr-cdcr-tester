f2a1b209779b70a5e962512a002844b3c8289780
sarkaramrit2 <amrit@ebctechnologies.com>
2018-03-23 21:34:32 +0530

    X-5

--- src/main/java/org/apache/solr/UltimateCdcrTesterV2.java
+++ src/main/java/org/apache/solr/UltimateCdcrTesterV2.java
@@ -64,11 +64,62 @@ public class UltimateCdcrTesterV2 {
 
         // first level sanity check
         {
+            for (int i=0; i<2; i++) { // 2 iterations, toggles
+                // index one doc
+                SolrInputDocument doc1 = new SolrInputDocument();
+                String id1 = UUID.randomUUID().toString();
+                doc1.addField("id", id1);
+                doc1.addField(FIELDS[0], strings[r.nextInt(5) % 5]);
+                doc1.addField(FIELDS[1], strings[r.nextInt(5) % 5]);
+
+                SolrInputDocument doc2 = new SolrInputDocument();
+                String id2 = UUID.randomUUID().toString();
+                doc2.addField("id", id2);
+                doc2.addField(FIELDS[0], strings[r.nextInt(5) % 5]);
+                doc2.addField(FIELDS[1], strings[r.nextInt(5) % 5]);
+
+                updateRequest = new UpdateRequest();
+                updateRequest.add(index(Arrays.asList(new SolrInputDocument[]{doc1, doc2})));
+
+                System.out.println("index: " + updateRequest);
+                source_cli.request(updateRequest, source_col);
+                updateRequest.commit(source_cli, source_col);
+                waitForSync(source_cli, target_cli, ALL);
+
+                // delete by id
+                deleteById(source_cli, target_cli, source_col);
+
+                // delete by query
+                deleteByQuery(source_cli, target_cli, source_col);
+
+                // toggle source and target
+                {
+                    source_cli.close();
+                    target_cli.close();
+                    if (source_zkHost.equals(C1_ZK)) {
+                        // make C2_ZK primary
+                        source_zkHost = C2_ZK;
+                        source_col = col2;
+                        target_zkHost = C1_ZK;
+                        target_col = col1;
+                    } else {
+                        // make C1_ZK primary
+                        source_zkHost = C1_ZK;
+                        source_col = col1;
+                        target_zkHost = C2_ZK;
+                        target_col = col2;
+                    }
+                    System.out.println("toggle direction: " + source_col + " | " + target_col);
+
+                    source_cli = new CloudSolrClient.Builder().withZkHost(source_zkHost).build();
+                    source_cli.setDefaultCollection(source_col);
+                    target_cli = new CloudSolrClient.Builder().withZkHost(target_zkHost).build();
+                    source_cli.setDefaultCollection(target_col);
+                }
+            }
         }
 
         for (int j = 0; j < 200 * Integer.parseInt(args[2]); j++) {
-            //while (true) {
-            List<SolrInputDocument> docs = new ArrayList<>();
             int action = r.nextInt(4) % 4;
             // 50% of probability of indexing, and 25%% of other two ops:
             // delete-by-id and delete-by-query
@@ -102,74 +153,19 @@ public class UltimateCdcrTesterV2 {
 
                 case 0:
                     // delete-by-id
-                    String fieldName = FIELDS[r.nextInt(2) % 2];
-                    String fieldValue = strings[r.nextInt(5) % 5];
-                    String payload = fieldName + ":" + fieldValue;
-                    QueryResponse source_resp = source_cli.query(new SolrQuery(payload));
-                    updateRequest = new UpdateRequest();
-                    if (source_resp.getResults().getNumFound() > 0) {
-                        String idToDel = source_resp.getResults().get(0).get("id").toString();
-                        if (idToDel.contains("parent_") || idToDel.contains("child_")) {
-                            //nested document, delete both parent and child to avoid linging docs
-                            if (idToDel.contains("parent_")) {
-                                idToDel = idToDel.substring("parent_".length());
-                            } else {
-                                idToDel = idToDel.substring("child_".length());
-                            }
-                            updateRequest.deleteById(Arrays.asList(new String[]{"parent_" + idToDel, "child_" + idToDel}));
-                            // update payload to verify on other cluster
-                            payload = "id:" + updateRequest.getDeleteById().get(0) + "OR" + updateRequest.getDeleteById().get(1);
-                        } else { //singlular document
-                            updateRequest.deleteById(idToDel);
-                            // update payload to verify on other cluster
-                            payload = "id:" + updateRequest.getDeleteById().get(0);
-                        }
-                        source_cli.request(updateRequest, source_col);
-                        updateRequest.commit(source_cli, source_col);
-
-                        waitForSync(source_cli, target_cli, payload);
-                    }
+                    deleteById(source_cli, target_cli, source_col);
                     break;
 
                 case 1:
                     // delete-by-query (delete both parent and child doc)
-                    updateRequest = new UpdateRequest();
-                    String fieldName1 = FIELDS[r.nextInt(2) % 2];
-                    String fieldValue1 = strings[r.nextInt(5) % 5];
-                    String payload1 = fieldName1 + ":" + fieldValue1;
-                    updateRequest.deleteByQuery(payload1);
-                    System.out.println("deleteByQuery: " + updateRequest);
-
-                    source_cli.request(updateRequest, source_col);
-                    updateRequest.commit(source_cli, source_col);
-
-                    waitForSync(source_cli, target_cli, payload1);
+                    deleteByQuery(source_cli, target_cli, source_col);
                     break;
 
                 default:
                     // index
+                    List<SolrInputDocument> docs = new ArrayList<>();
                     for (int i = 0; i < 1024 * Integer.parseInt(args[3]); i++) {
-                        SolrInputDocument document = new SolrInputDocument();
-                        String id = UUID.randomUUID().toString();
-                        document.addField("id", id);
-                        document.addField("member_id_i", r.nextInt(5) % 5);
-                        document.addField("subtotal_i", 1024 + r.nextInt(5) % 5);
-                        document.addField("quantity_l", Math.abs(r.nextLong() % 5));
-                        String order = strings[r.nextInt(5) % 5];
-                        String ship_addr = strings[r.nextInt(5) % 5];
-                        document.addField("order_no_s", order);
-                        document.addField("ship_addr_s", ship_addr);
-                        if (r.nextBoolean()) { // with child doc
-                            SolrInputDocument childdoc = new SolrInputDocument();
-                            childdoc.addField("id", "child_" + id);
-                            childdoc.addField("order_no_s", order);
-                            childdoc.addField("ship_addr_s", ship_addr);
-                            document.addChildDocument(childdoc);
-                            document.addField("id", "parent_" + id);
-                        } else {
-                            document.addField("id", id); // no child doc
-                        }
-                        docs.add(document);
+                        docs = index(docs);
                     }
                     updateRequest = new UpdateRequest();
                     updateRequest.add(docs);
@@ -189,6 +185,81 @@ public class UltimateCdcrTesterV2 {
         System.out.println("total time spend :: " + (end_time - curr_time));
 
         System.exit(0);
+    }
+
+    // DBI
+    private static void  deleteById(CloudSolrClient source_cli, CloudSolrClient target_cli, String source_col)
+            throws Exception {
+        UpdateRequest updateRequest = new UpdateRequest();
+        String fieldName = FIELDS[r.nextInt(2) % 2];
+        String fieldValue = strings[r.nextInt(5) % 5];
+        String payload = fieldName + ":" + fieldValue;
+        QueryResponse source_resp = source_cli.query(new SolrQuery(payload));
+        if (source_resp.getResults().getNumFound() > 0) {
+            String idToDel = source_resp.getResults().get(0).get("id").toString();
+            if (idToDel.contains("parent_") || idToDel.contains("child_")) {
+                //nested document, delete both parent and child to avoid linging docs
+                if (idToDel.contains("parent_")) {
+                    idToDel = idToDel.substring("parent_".length());
+                } else {
+                    idToDel = idToDel.substring("child_".length());
+                }
+                updateRequest.deleteById(Arrays.asList(new String[]{"parent_" + idToDel, "child_" + idToDel}));
+                // update payload to verify on other cluster
+                payload = "id:" + updateRequest.getDeleteById().get(0) + "OR" + updateRequest.getDeleteById().get(1);
+            } else { //singlular document
+                updateRequest.deleteById(idToDel);
+                // update payload to verify on other cluster
+                payload = "id:" + updateRequest.getDeleteById().get(0);
+            }
+            source_cli.request(updateRequest, source_col);
+            updateRequest.commit(source_cli, source_col);
+
+            waitForSync(source_cli, target_cli, payload);
+        }
+
+    }
+
+    // DBQ
+    private static void deleteByQuery(CloudSolrClient source_cli, CloudSolrClient target_cli, String source_col)
+            throws Exception {
+        UpdateRequest updateRequest = new UpdateRequest();
+        String fieldName1 = FIELDS[r.nextInt(2) % 2];
+        String fieldValue1 = strings[r.nextInt(5) % 5];
+        String payload1 = fieldName1 + ":" + fieldValue1;
+        updateRequest.deleteByQuery(payload1);
+        System.out.println("deleteByQuery: " + updateRequest);
+
+        source_cli.request(updateRequest, source_col);
+        updateRequest.commit(source_cli, source_col);
+
+        waitForSync(source_cli, target_cli, payload1);
+    }
+
+    // add-update
+    private static List<SolrInputDocument> index(List<SolrInputDocument> docs) {
+        SolrInputDocument document = new SolrInputDocument();
+        String id = UUID.randomUUID().toString();
+        document.addField("id", id);
+        document.addField("member_id_i", r.nextInt(5) % 5);
+        document.addField("subtotal_i", 1024 + r.nextInt(5) % 5);
+        document.addField("quantity_l", Math.abs(r.nextLong() % 5));
+        String order = strings[r.nextInt(5) % 5];
+        String ship_addr = strings[r.nextInt(5) % 5];
+        document.addField(FIELDS[0], order);
+        document.addField(FIELDS[1], ship_addr);
+        if (r.nextBoolean()) { // with child doc
+            SolrInputDocument childdoc = new SolrInputDocument();
+            childdoc.addField("id", "child_" + id);
+            childdoc.addField(FIELDS[0], order);
+            childdoc.addField(FIELDS[1], ship_addr);
+            document.addChildDocument(childdoc);
+            document.addField("id", "parent_" + id);
+        } else {
+            document.addField("id", id); // no child doc
+        }
+        docs.add(document);
+        return docs;
     }
 
     // create sentences for data to index
